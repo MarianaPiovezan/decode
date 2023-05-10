110d1f179033c2cdf108cddc0a13a8b5af685a62
Mariana Piovezan <piovmari@gmail.com>
2023-05-10 14:01:20 -0300

    feat: add decrypty function

---
+++ js/decrypty.js
@@ -0,0 +1,29 @@
+import { textarea, resultText, decodeEmpty, decodeResult, error } from "./encrypty";
+
+
+function decrypty(stringEncrypty) {
+    const keys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+
+    stringEncrypty = stringEncrypty.toLowerCase();
+
+    for (let i = 0; i < keys.length; i++) {
+        if (stringEncrypty.includes(keys[i][1])) {
+            stringEncrypty = stringEncrypty.replaceAll(keys[i][1], keys[i][0]);
+
+        }
+    }
+    return stringEncrypty;
+};
+
+function btnDecrypty() {
+    const textDecrypty = decrypty(resultText);
+
+    
+        resultText.innerHTML = `${textDecrypty}`;
+        textarea.value = "";
+        decodeResult.style.display = "flex";
+        decodeEmpty.style.display = "none";
+        error.style.display = "none";  
+
+};
+
